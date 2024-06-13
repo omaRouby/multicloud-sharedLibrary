fa7ebb372a0eb39ef377943302e607d0f7f8e7b0
Omar Ahmed Rouby <164004472+omaRouby@users.noreply.github.com>
2024-06-13 14:43:43 +0300

    Update deployToOpenShift.groovy

--- vars/deployToOpenShift.groovy
+++ vars/deployToOpenShift.groovy
@@ -5,6 +5,6 @@ def call(String OPENSHIFT_CREDENTIALS_ID, String OPENSHIFT_PROJECT, String CLUST
     }
 
     // Apply all YAML files in the 'openshift' directory to the OpenShift cluster
-    // sh "oc apply -f openshift --namespace=${OPENSHIFT_PROJECT}"
-    sh "oc new-app omarrouby/ivolve-app:4 --namespace=${OPENSHIFT_PROJECT}"
+    sh "oc apply -f openshift --namespace=${OPENSHIFT_PROJECT}"
+    
 }
