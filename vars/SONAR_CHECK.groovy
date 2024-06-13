69ccd073ddc492ba7fc02bd332c77a6e206e3997
Omar Ahmed Rouby <164004472+omaRouby@users.noreply.github.com>
2024-06-13 11:40:47 +0300

    Update SONAR_CHECK.groovy

--- vars/SONAR_CHECK.groovy
+++ vars/SONAR_CHECK.groovy
@@ -1,9 +1,12 @@
-#!usr/bin/env groovy
-def call(){ 
-	echo "Running SonarQube "
-	withSonarQubeEnv(credentialsId: 'sonar') {
-		echo "Running SonarQube Analysis..."
-		sh ''' $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=MULTICLOUD \
-		-Dsonar.java.binaries=. \
-		-Dsonar.projectKey=MULTICLOUD '''
-	}
+#!/usr/bin/env groovy
+def call() { 
+    echo "Running SonarQube"
+    withSonarQubeEnv(credentialsId: 'sonar') {
+        echo "Running SonarQube Analysis..."
+        sh ''' 
+        $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=MULTICLOUD \
+        -Dsonar.java.binaries=. \
+        -Dsonar.projectKey=MULTICLOUD 
+        '''
+    }
+}
